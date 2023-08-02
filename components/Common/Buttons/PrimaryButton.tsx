fa9eebc8f099e488d0e586462c536c9a4471733d
memokey <skytopground@gmail.com>
2023-08-02 08:38:00 -0700

    changed button animation

--- components/Common/Buttons/PrimaryButton.tsx
+++ components/Common/Buttons/PrimaryButton.tsx
@@ -15,7 +15,7 @@ const PrimaryButton = (props: PrimaryButtonProps) => {
       className={`font-medium py-[16px] px-[32px] rounded-[4px] text-white h-[50px] text-[16px] sm:text-[18px] text-center tracking-wider inline-flex items-center justify-center ${
         props.disabled
           ? 'bg-[#1d1e20] button-disabled'
-          : 'bg-primary shadow-[-5px_5px_0px_#E33838] hover:shadow-[0px_0px_0px_#E33838] transition-shadow duration-1000'
+          : 'bg-primary shadow-[-5px_5px_0px_#E33838] hover:shadow-[0px_0px_0px_#E33838] transition-shadow ease-out duration-1000'
       } ${props.styles}`}
       onClick={props.disabled ? null : props.onClick}
     >
